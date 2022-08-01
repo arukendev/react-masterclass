213fe4157c6a73a37883ac1b03e29ccb01fa79c5
youngkaya <iamjaehyun4@gmail.com>
2022-08-01 20:41:36 +0900

    login form with react-hook-form

--- src/theme.ts
+++ src/theme.ts
@@ -6,10 +6,3 @@ export const darkTheme: DefaultTheme = {
   accentColor: "#e1b12c",
   cardBgColor: "transparent",
 };
-
-export const lightTheme: DefaultTheme = {
-  bgColor: "whitesmoke",
-  textColor: "black",
-  accentColor: "#e1b12c",
-  cardBgColor: "white",
-};
